2c45ad0ac45e2c365203264958452abe7ef1d891
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2018-02-09 22:29:26 +0100

    buildman: support newer gcc versions from kernel.org
    Add support for gcc versions 6.4.0 and 4.9.4.
    
    Also use a regex for matching the tarball names. Some gcc versions
    use '-ARCH-' instead of '_ARCH-'.
    
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
    
    Series-to: u-boot
    Series-cc: trini
    Series-cc: sjg

--- tools/buildman/toolchain.py
+++ tools/buildman/toolchain.py
@@ -33,7 +33,7 @@ class MyHTMLParser(HTMLParser):
         HTMLParser.__init__(self)
         self.arch_link = None
         self.links = []
-        self._match = '_%s-' % arch
+        self.re_arch = re.compile('[-_]%s-' % arch)
 
     def handle_starttag(self, tag, attrs):
         if tag == 'a':
@@ -41,7 +41,7 @@ class MyHTMLParser(HTMLParser):
                 if tag == 'href':
                     if value and value.endswith('.xz'):
                         self.links.append(value)
-                        if self._match in value:
+                        if self.re_arch.search(value):
                             self.arch_link = value
 
 
@@ -431,7 +431,7 @@ class Toolchains:
         """
         arch = command.OutputOneLine('uname', '-m')
         base = 'https://www.kernel.org/pub/tools/crosstool/files/bin'
-        versions = ['4.9.0', '4.6.3', '4.6.2', '4.5.1', '4.2.4']
+        versions = ['6.4.0', '5.5.0', '4.9.4', '4.9.0', '4.6.3', '4.6.2', '4.5.1', '4.2.4']
         links = []
         for version in versions:
             url = '%s/%s/%s/' % (base, arch, version)
